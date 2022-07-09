9b50f7fb485ab4543220050ca4a83a0d0ee8efcd
ejones9 <52759445+ejones9@users.noreply.github.com>
2022-07-09 15:28:02 -0400

    Update GPU_CNN_no_batching_images_only.py

--- GPU_CNN_no_batching_images_only.py
+++ GPU_CNN_no_batching_images_only.py
@@ -9,8 +9,8 @@ gpus = tf.config.list_physical_devices('GPU')
 if gpus:
       # Restrict TensorFlow to only allocate 10GB of memory on the first GPU
      try:
-         tf.config.experimental.set_virtual_device_configuration( gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=300)])
-         tf.config.experimental.set_virtual_device_configuration( gpus[1], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=300)])
+         tf.config.experimental.set_virtual_device_configuration( gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=30000)])
+         tf.config.experimental.set_virtual_device_configuration( gpus[1], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=30000)])
          logical_gpus = tf.config.experimental.list_logical_devices('GPU')
          #print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
                              
