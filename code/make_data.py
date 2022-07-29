4d92b478ea5e518523bb632d841b4035c4b3a1be
Tuan Do <tdo@astro.ucla.edu>
2022-07-28 21:33:39 -0700

    code to create smaller subsets of the full HDF file image files for testing architectures

--- code/make_data.py
+++ code/make_data.py
@@ -1,8 +1,9 @@
-import sys
+import sys,os
 import numpy as np
 import pandas as pd
 import photoz_utils
-
+import random
+import h5py
 
 
 def make_hsc_v5(inputfile='/mnt/data/HSC/HSC_v3/all_specz_flag_forced_forced2_spec_z_matched_online.csv',
@@ -133,3 +134,38 @@ def make_hsc_v6(inputfile='/mnt/data/HSC/HSC_v3/all_specz_flag_forced_forced2_sp
     print('outputfile: '+outputfile)
     photozdata.to_csv(outputfile,index=None)
     
+
+def make_hsc_v6_small_hdf(ntrain=10000,ntest=2000,nvalidation=2000):
+    '''
+    Create a smaller images dataset for easier training and testing. 
+
+
+    '''
+    sample_sizes = [ntrain,ntest,nvalidation]
+
+    inputfiles = ['five_band_image127x127_training_with_metadata.hdf5','five_band_image127x127_testing_with_metadata.hdf5','five_band_image127x127_validation_with_metadata.hdf5']
+    directory = '/data/HSC/HSC_v6/step2A/127x127/'
+    for i in range(len(sample_sizes)):
+        current_file = os.path.join(directory,inputfiles[i])
+        nsample = sample_sizes[i]
+        print(current_file,'nsamples',nsample)
+        hf = h5py.File(current_file,'r')
+        
+        inds = np.sort(random.sample(list(np.arange(len(hf['object_id']))),sample_sizes[i]))
+
+        part = os.path.splitext(current_file)
+        outfile = part[0]+'_small.hdf5'
+        
+
+        f = h5py.File(outfile,'w')
+        print('output',outfile)
+        for k in hf.keys():
+
+            tmp = hf[k]
+            s = list(np.shape(tmp))
+            s[0] = nsample
+            print(k,nsample,tmp.dtype)            
+            dset = f.create_dataset(k,shape=s,dtype=tmp.dtype)
+            dset.write_direct(tmp[inds])
+        f.close()
+        hf.close()
