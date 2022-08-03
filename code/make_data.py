82d5fe61d92f6b279273f72945e8a22c9639b18c
YunqiBillyLi <107442872+YunqiBillyLi@users.noreply.github.com>
2022-08-02 21:25:36 -0700

    Update make_data.py

--- code/make_data.py
+++ code/make_data.py
@@ -271,3 +271,52 @@ def make_hdf5_from_raw_images():
 
   hf.close()
 
+def make_hsc_v6_small_hdf_single(ntrain=10000,ntest=2000,nvalidation=2000):
+    '''
+    Create a smaller images dataset for easier training and testing. 
+
+
+    '''
+    sample_sizes = [ntrain,ntest,nvalidation]
+
+    inputfiles = ['five_band_image127x127_with_metadata_corrected.hdf5']
+    directory = '/data/HSC/HSC_v6/step2A/127x127/'
+    current_file = os.path.join(directory,inputfiles[0])
+    ntrain = sample_sizes[0]
+    ntest = sample_sizes[1]
+    nvalidation = sample_sizes[2]
+    print(current_file,'nsamples',nsample)
+    hf = h5py.File(current_file,'r')
+        
+    inds = random.sample(list(np.arange(len(hf['object_id']))),sample_sizes[0]+sample_sizes[1]+sample_sizes[2])
+    inds_train = inds[:ntrain]
+    inds_test = inds[ntrain:ntrain+ntest]
+    inds_validation = inds[ntrain+ntest:]
+
+    part = os.path.splitext(current_file)
+    outfile_train = part[0]+'training_small.hdf5'
+    outfile_test = part[0]+'testing_small.hdf5'
+    outfile_validation = part[0]+'validation_small.hdf5'
+        
+
+    f_train = h5py.File(outfile_train,'w')
+    f_test = h5py.File(outfile_test,'w')
+    f_validation = h5py.File(outfile_validation,'w')
+    print('output',outfile)
+    for k in hf.keys():
+
+        tmp = hf[k]
+        s_train = list(np.shape(tmp))
+        s_test = list(np.shape(tmp))
+        s_validation = list(np.shape(tmp))
+        s_train[0] = ntrain
+        s_test[0] = ntest
+        s_validation[0] = nvalidation           
+        trainset = f_train.create_dataset(k,shape=s_train,dtype=tmp.dtype)
+        testset = f_test.create_dataset(k,shape=s_test,dtype=tmp.dtype)
+        validationset = f_validation.create_dataset(k,shape=s_validation,dtype=tmp.dtype)
+        trainset.write_direct(tmp[inds_train])
+        testset.write_direct(tmp[inds_test])
+        validationset.write_direct(tmp[inds_validation])
+    f.close()
+    hf.close()
